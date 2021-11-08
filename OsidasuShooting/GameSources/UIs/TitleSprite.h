c57deca1db3c500a68539043d4c867ad14afd2b1
Ryushiro Sato <zeal20300167@fsgcl.jp>
2021-11-09 02:27:10 +0900

    TitleSpriteクラスを追加した

---
+++ OsidasuShooting/GameSources/UIs/TitleSprite.h
@@ -0,0 +1,33 @@
+/*!
+@file TitleSprite.h
+@brief ƒ^ƒCƒgƒ‹
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross 
+{
+	class TitleSprite : public GameObject {
+		bool m_trace;
+		Vec2 m_startScale;
+		Vec2 m_startPos;
+		wstring m_textureKey;	
+
+	public:
+		TitleSprite(const shared_ptr<Stage>& stage,const wstring& TextureKey,bool Trace,
+			const Vec2& StartScale,const Vec2& StartPos) :
+			GameObject(stage),
+			m_textureKey(TextureKey),
+			m_trace(Trace),
+			m_startScale(StartScale),
+			m_startPos(StartPos)
+		{}
+			
+		virtual ~TitleSprite() {};
+
+		void OnCreate() ;
+		void OnUpdate() {};
+
+	};
+}
