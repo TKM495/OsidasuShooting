c1565fe2d82bdbbac573c10badc69c3d202cd16e
Takumi Watanabe <zeal20300166@fsgcl.jp>
2021-11-10 11:28:12 +0900

    一定時間ホバーができるようになった。

--- OsidasuShooting/GameSources/Utility/GroundingDecision.cpp
+++ OsidasuShooting/GameSources/Utility/GroundingDecision.cpp
@@ -1,14 +1,48 @@
+/*!
+@file   GroundingDecision.cpp
+@brief  接地判定クラスの実体
+*/
+
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross {
+	// しきい値の実体
+	float GroundingDecision::m_threshold = 0.1f;
+
+	GroundingDecision::GroundingDecision()
+		:m_radius(0.0f), m_isInit(false)
+	{}
+
+	GroundingDecision::GroundingDecision(float radius)
+		: m_radius(radius), m_isInit(true)
+	{}
+
+	GroundingDecision::GroundingDecision(const Vec3& scale)
+		: m_isInit(true)
+	{
+		m_radius = scale.y / 2.0f;
+	}
+
 	bool GroundingDecision::Calculate(const Vec3& pos) {
+		if (!m_isInit) {
+			throw BaseException(
+				L"初期化されていません。",
+				L"if (!m_isInit)",
+				L"GroundingDecision::Calculate()"
+			);
+		}
+		// ステージ上のすべてのゲームオブジェクトを取得
 		auto objs = App::GetApp()->GetScene<Scene>()->GetActiveStage()->GetGameObjectVec();
 		for (auto obj : objs) {
+			// CollisionObbコンポーネントがある場合
 			auto ColObb = obj->GetComponent<CollisionObb>(false);
 			if (ColObb) {
 				auto Obb = ColObb->GetObb();
-				if (HitTest::SEGMENT_OBB(pos, pos + Vec3(0.0f, -1.0f, 0.0f), Obb)) {
+				// OBBと線分の衝突判定
+				if (HitTest::SEGMENT_OBB(pos,
+					pos + Vec3(0.0f, -(m_radius + m_threshold), 0.0f), Obb))
+				{
 					return true;
 				}
 			}
--- OsidasuShooting/GameSources/Utility/GroundingDecision.h
+++ OsidasuShooting/GameSources/Utility/GroundingDecision.h
@@ -1,9 +1,50 @@
+/*!
+@file   GroundingDecision.h
+@brief  接地判定クラス
+*/
+
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
-	static class GroundingDecision {
+	class GroundingDecision {
+		// オブジェクトの半径
+		float m_radius;
+		// しきい値
+		static float m_threshold;
+		// 初期化したか
+		bool m_isInit;
 	public:
-		static bool Calculate(const Vec3& pos);
+		// コンストラクタ
+		GroundingDecision();
+		GroundingDecision(float radius);
+		GroundingDecision(const Vec3& scale);
+
+		/**
+		 * @brief 判定
+		 *
+		 * @param pos 自身の位置
+		 * @return trueなら接地している
+		 */
+		bool Calculate(const Vec3& pos);
+
+		/**
+		 * @brief 自身の半径の設定
+		 *
+		 * @param radius 半径
+		 */
+		void SetRadius(float radius) {
+			m_radius = radius;
+			m_isInit = true;
+		}
+		/**
+		 * @brief 自身の半径の設定
+		 *
+		 * @param scale 自身のスケール
+		 */
+		void SetRadius(const Vec3& scale) {
+			m_radius = scale.y / 2.0f;
+			m_isInit = true;
+		}
 	};
 }
