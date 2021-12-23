467e28ab7e7b3a10b505ab05f7de6cd8130ba2cf
Takumi Watanabe <zeal20300166@fsgcl.jp>
2021-12-23 12:27:26 +0900

    空中と地上で抵抗の値を変えてより自然な放物線を描くようにした。 最大速度を定義し、一定の速度より速くなることを防ぐようにした。

--- OsidasuShooting/GameSources/Component/PhysicalBehavior.cpp
+++ OsidasuShooting/GameSources/Component/PhysicalBehavior.cpp
@@ -7,6 +7,11 @@
 #include "Project.h"
 
 namespace basecross {
+	void PhysicalBehavior::OnCreate() {
+		m_groundingDecision.reset(new GroundingDecision(GetGameObject()->GetComponent<Transform>()->GetScale()));
+		m_groundingDecision->AddNotDecisionTag(L"Bomb");
+		m_groundingDecision->AddNotDecisionTag(L"Bullet");
+	}
 	void PhysicalBehavior::OnUpdate() {
 		// トランスフォームコンポーネントの取得
 		auto transComp = GetGameObject()->GetComponent<Transform>();
@@ -17,12 +22,19 @@ namespace basecross {
 		pos += m_velocity * delta;
 		transComp->SetPosition(pos);
 
+		// 接地している場合は地面の抵抗：逆は空気の抵抗を使用
+		auto drag = m_groundingDecision->Calculate(pos) ? m_groundDrag : m_airDrag;
+
 		// 移動方向の逆に抵抗力（摩擦など）を加える
-		m_velocity += -m_velocity * m_drag * delta;
+		m_velocity += -m_velocity * drag * delta;
 
 		// 無駄な計算を防ぐためしきい値以下は0とみなす
 		if (m_velocity.lengthSqr() < m_threshold * m_threshold)
 			m_velocity = Vec3(0.0f);
+
+		// 現在の速度が最大速度より大きい場合はクランプする
+		if (m_velocity.lengthSqr() > m_maxSpeed * m_maxSpeed)
+			m_velocity = Utility::ChangeVectorLength(m_velocity, m_maxSpeed);
 	}
 
 	void PhysicalBehavior::Move(const Vec3& force) {
