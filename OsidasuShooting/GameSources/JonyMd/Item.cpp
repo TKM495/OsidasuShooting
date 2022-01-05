b0d55675a9a04bcdaf5073dafcfb5b6dfc0b0728
JONY MD <vondo.manob@gmail.com>
2022-01-05 09:23:23 +0900

    アイテムを一定時間後に消す処理

--- OsidasuShooting/GameSources/JonyMd/Item.cpp
+++ OsidasuShooting/GameSources/JonyMd/Item.cpp
@@ -15,6 +15,73 @@ namespace basecross {
 		auto gravity = AddComponent<Gravity>();
 		auto collision = AddComponent<CollisionObb>();
 
+	}
+
+
+
+	void Item::OnUpdate()
+	{
+		auto& app = App::GetApp();
+		float deltaTime = app->GetElapsedTime();
+
+		if (stayTime > 0)
+		{
+			stayTime -= deltaTime;//　泊まる時間
+			return;
+		}
+		//泊まるが時間が終わったら点滅が始まります
+
+
+
+		//点滅の処理
+		blinkingTime -= deltaTime;
+		timeChecker += deltaTime;
+		float colorAdjustment;
+
+		if (timeChecker < blinkFadeInTime)
+		{
+			colorAdjustment = timeChecker / blinkFadeInTime;
+		}
+		else if (timeChecker < (blinkFadeInTime + blinkStayTime))
+		{
+			colorAdjustment = 1.0f;
+		}
+		else if (timeChecker < (blinkFadeInTime + blinkStayTime + blinkFadeOutTime))
+		{
+			colorAdjustment = 1.0f - (timeChecker - (blinkFadeInTime + blinkStayTime)) / blinkFadeOutTime;
+		}
+		else
+		{
+			timeChecker = 0;
+			colorAdjustment = 0.0f;
+
+			// 毎回点滅が終わったら点滅の速度が増える
+			blinkingSpeed += 0.0001f;
+			blinkFadeInTime -= blinkingSpeed;
+			blinkFadeOutTime -= blinkingSpeed;
+
+			if (blinkFadeInTime < 0)
+			{
+				blinkFadeInTime = blinkingSpeed;
+			}
+			if (blinkFadeOutTime < 0)
+			{
+				blinkFadeOutTime = blinkingSpeed;
+			}
+			// 毎回点滅が終わったら点滅の速度が増える..終了
+		}
+
+		SetAdjustColor(colorAdjustment);
+		//点滅の処理..終了
+
+
+		//　点滅の時間が終わったら物体が消える
+		if (blinkingTime < 0)
+		{
+			GetStage()->RemoveGameObject<Item>(GetThis<Item>());
+		}
+
+
 
 	}
 }
--- OsidasuShooting/GameSources/JonyMd/Item.h
+++ OsidasuShooting/GameSources/JonyMd/Item.h
@@ -5,6 +5,26 @@ namespace basecross {
 
 	class Item : public GameObject
 	{
+		// (一定時間)秒として表示されています。
+		float stayTime = 3.0f;//　物体の泊まる時間
+
+		//点滅用
+		//全　数字　は秒として表示されています。
+		float blinkingTime = 3.0f;//　物体の点滅する時間
+
+		float blinkFadeInTime = 0.05f;
+		float blinkStayTime = 0.01f;
+		float blinkFadeOutTime = 0.10f;
+		float timeChecker = blinkFadeInTime + blinkStayTime; //blinkFadeOutTime　から始まる	
+		float blinkingSpeed = 0.002f;
+
+		void SetAdjustColor(float colorValue)
+		{
+			auto component = GetComponent<BcPNTStaticDraw>();
+			Col4 color = Col4(colorValue, colorValue, colorValue, 0.0f);
+			component->SetDiffuse(color);
+		}
+		//点滅用..終了
 
 	public:
 		Item(const shared_ptr<Stage>& stage)
@@ -12,6 +32,7 @@ namespace basecross {
 		{}
 
 		void OnCreate() override;
+		void OnUpdate() override;
 
 
 		enum class ItemType {
